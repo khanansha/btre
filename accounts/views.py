63abc9078745b0d63d31bf5a0fcd884e78cb28a7
ansha <anjumkhan88987@gmail.com>
2020-04-11 12:34:58 +0530

    reset password complated

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,17 @@ from django.contrib import messages, auth
 
 from django.contrib.auth.models import User
 from contacts.models import Contact
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.core.validators import validate_email
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.utils.crypto import get_random_string
+from django.core.mail import send_mail
+from django.utils.encoding import force_bytes, force_text
+
 # Create your views here.
 
 
@@ -74,6 +85,7 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(username=username, password=password)
+        
 
         if user is not None:
 
@@ -117,3 +129,31 @@ def dashboard(request):
     }
 
     return render(request, 'accounts/dashboard.html', context)
+
+def rest_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        randotp = get_random_string(6, allowed_chars='0123456789')
+        if User.objects.filter(email=email).exists():
+            #UPDATE User table SET password = 'randopt' WHERE email='email'
+            #u=User.objects.filter(email=email).update(password=randotp)
+            u = User.objects.get(email=email)
+            u.set_password(randotp)
+            u.save()
+            current_site = get_current_site(request)
+            subject, from_email, to = 'reset password', 'anjumkhan88987@gmail.com', email
+            text_content = ''
+            # html_content = 'Please activate your account <a href="http://' + \
+            # current_site.domain + '/activate_email?e='+to+'" target="_blank">Click here</a>'
+            html_content = ' Hii your new password is ' + randotp + ' now you can login with this password'
+            msg = EmailMultiAlternatives(
+                subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            messages.success(request, 'we have sent you an email please follow the instructions')
+            return redirect('login')
+        else: 
+            messages.error(request, 'Invaild email')
+            return redirect('rest_password')
+    return render (request,'accounts/forget_password.html')
