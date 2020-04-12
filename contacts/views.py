081229a11ee943e51c2296cc4eeb8ed7e411e937
ansha <anjumkhan88987@gmail.com>
2020-04-12 20:08:15 +0530

    add enquriy count logic

--- contacts/views.py
+++ contacts/views.py
@@ -1,55 +1,95 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Contact
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
+from accounts.models import Pakage
 # Create your views here.
-#@login_required(login_url="/accounts/register")
+# @login_required(login_url="/accounts/register")
 
 
 def contact(request):
     if request.method == 'POST':
-      listing_id = request.POST['listing_id']
+        listing_id = request.POST['listing_id']
 
-      listing = request.POST['listing']
+        listing = request.POST['listing']
 
-      name = request.POST['name']
+        name = request.POST['name']
 
-      email = request.POST['email']
+        email = request.POST['email']
 
-      phone = request.POST['phone']
+        phone = request.POST['phone']
 
-      message = request.POST['message']
+        message = request.POST['message']
 
-      user_id = request.POST['user_id']
+        user_id = request.POST['user_id']
 
-      realtor_email = request.POST['realtor_email']
+        realtor_email = request.POST['realtor_email']
         #  Check if user has made inquiry already
 
-      if request.user.is_authenticated:
+        if request.user.is_authenticated:
 
-        user_id = request.user.id
+            user_id = request.user.id
 
-        has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
 
-        if has_contacted:
+            if has_contacted:
 
-          messages.error(request, 'You have already made an inquiry for this listing')
+                messages.error(
+                    request, 'You have already made an inquiry for this listing')
 
-          return redirect('/listings/'+listing_id)
+                return redirect('/listings/'+listing_id)
+            # fetch package FILTER (user_id= user_id)
+            contact_count = Contact.objects.filter(user_id=user_id).count()
+            count = str(contact_count)
+            print(count)
+            package = Pakage.objects.filter(user_id=user_id)
+            package_count = package[0].pakage
+            print(package_count)
+            # get total number of enquiry count
+            if count < package_count:
+              # if 0< 2 (true)
+              # ==
+              # !=
+              # <= less than equal to
+              # >= greater than equal to
+              # <
+              # >
+                contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                                  email=email, phone=phone, message=message, user_id=user_id)
 
-
-
-    contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
-
-    contact.save()
+                contact.save()
     # Send email
 
-    send_mail('Property Listing Inquiry', 'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info', 'anjumkhan88987@gmail.com', [realtor_email, email],
-    fail_silently=False )
+                send_mail('Property Listing Inquiry', 'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info', 'anjumkhan88987@gmail.com', [realtor_email, email],
+                          fail_silently=False)
+                messages.success(
+                    request, 'Your request has been submitted, a realtor will get back to you soon')
 
+                return redirect('/listings/'+listing_id)
+            else:
+                messages.error(
+                    request, 'expired')
 
-    messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+                return redirect('/listings/'+listing_id)
 
-    return redirect('/listings/'+listing_id)
+    # CONTACT.SAVE
+    # SEND EMAIL
+    # else
+    # MESSAGE.ERROR
+    # REDIRECT(LISTING)
+    # contact = Contact(listing=listing, listing_id=listing_id, name=name,
+     #                 email=email, phone=phone, message=message, user_id=user_id)
+
+    # contact.save()
+    # Send email
+
+    # send_mail('Property Listing Inquiry', 'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info', 'anjumkhan88987@gmail.com', [realtor_email, email],
+     #         fail_silently=False)
+
+    # messages.success(
+     #   request, 'Your request has been submitted, a realtor will get back to you soon')
+
+    # return redirect('/listings/'+listing_id)
